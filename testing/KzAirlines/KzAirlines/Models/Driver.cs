b99adef18049a5598737406da0878b40ac281641
RollandDin <daniil.latysh@gmail.com>
2018-12-26 10:20:32 +0300

    refactored_driver_to_singleton

--- testing/KzAirlines/KzAirlines/Models/Driver.cs
+++ testing/KzAirlines/KzAirlines/Models/Driver.cs
@@ -4,31 +4,24 @@ using System;
 
 namespace KzAirlines.Models
 {
-    public class Driver : IDisposable
+    public class Driver
     {
         public IWebDriver Instance;
 
-        public Driver()
-        {
-            if (Instance == null)
-            {
-                var dir = Environment.CurrentDirectory;
-                var chromeDriverService = FirefoxDriverService.CreateDefaultService(dir);
-                chromeDriverService.HideCommandPromptWindow = true;
+        private static readonly Lazy<Driver> lazy =
+        new Lazy<Driver>(() => new Driver());
 
-                Instance = new FirefoxDriver(chromeDriverService);
-                Instance.Manage().Timeouts().ImplicitWait.Add(TimeSpan.FromSeconds(2));
-                Instance.Manage().Window.Maximize();
-            }
-            else
-                throw new InvalidOperationException();
+        private Driver()
+        {
+            var dir = Environment.CurrentDirectory;
+            var chromeDriverService = FirefoxDriverService.CreateDefaultService(dir);
+            chromeDriverService.HideCommandPromptWindow = true;
+
+            Instance = new FirefoxDriver(chromeDriverService);
+            Instance.Manage().Timeouts().ImplicitWait.Add(TimeSpan.FromSeconds(2));
+            Instance.Manage().Window.Maximize();
         }
 
-        public void Dispose()
-        {
-            Instance.Quit();
-            Instance.Dispose();
-            Instance=null;
-        }
+        public static Driver GetInstance() => lazy.Value;
     }
 }
--- testing/KzAirlines/KzAirlines/Tests/BuyTicketsPageTests.cs
+++ testing/KzAirlines/KzAirlines/Tests/BuyTicketsPageTests.cs
@@ -10,83 +10,84 @@ namespace KzAirlines.Tests
         [TestMethod]
         public void OneCanFindTicketsFromMinskToMoscow()
         {
-            using (var driver = new Driver())
-            {
-                var page = new BuyTicketsPage(driver.Instance);
-                page.Open();
-                page.InitFrom("Minsk");
-                page.InitTo("Moscow");
-                Assert.IsTrue(page.ResultsFound());
-            }
+            var driver = Driver.GetInstance();
+
+            var page = new BuyTicketsPage(driver.Instance);
+            page.Open();
+            page.InitFrom("Minsk");
+            page.InitTo("Moscow");
+            Assert.IsTrue(page.ResultsFound());
+
         }
 
         [TestMethod]
         public void OneCantFindTicketsFromMinskToDenver()
         {
-            using (var driver = new Driver())
-            {
-                var page = new BuyTicketsPage(driver.Instance);
-                page.Open();
-                page.InitFrom("Minsk");
-                page.InitTo("Denver");
-                Assert.IsFalse(page.ResultsFound());
-            }
+
+            var driver = Driver.GetInstance();
+
+            var page = new BuyTicketsPage(driver.Instance);
+            page.Open();
+            page.InitFrom("Minsk");
+            page.InitTo("Denver");
+            Assert.IsFalse(page.ResultsFound());
+
         }
 
         [TestMethod]
         public void OneCanSpecifyBackDateWhenOneWayNotSelected()
         {
-            using (var driver = new Driver())
-            {
-                var page = new BuyTicketsPage(driver.Instance);
-                page.Open();
-                page.ClickOneWay();
-                Assert.IsTrue(page.IsReturnDateVisible());
-            }
+            var driver = Driver.GetInstance();
+
+            var page = new BuyTicketsPage(driver.Instance);
+            page.Open();
+            page.ClickOneWay();
+            Assert.IsTrue(page.IsReturnDateVisible());
+
         }
 
         [TestMethod]
         public void OneCantSpecifyBackDateWhenOneWaSelected()
         {
-            using (var driver = new Driver())
-            {
-                var page = new BuyTicketsPage(driver.Instance);
-                page.Open();
-                Assert.IsFalse(page.IsReturnDateVisible());
-            }
+            var driver = Driver.GetInstance();
+
+            var page = new BuyTicketsPage(driver.Instance);
+            page.Open();
+            Assert.IsFalse(page.IsReturnDateVisible());
+
         }
 
         [TestMethod]
         public void OneCanLogin()
         {
-            using (var driver = new Driver())
-            {
-                var page = new BuyTicketsPage(driver.Instance);
-                page.Open();
-                page.ClickLoginButton();
-                page.EnterLogin("kadac28@gmail.com");
-                page.EnterPassword("kdkddkkd1211");
-                page.ClickLoginButton();
-                Assert.AreEqual(expected: "ANDREY", actual: page.UserName);
-                Assert.IsTrue(page.Authorized);
-            }
+            var driver = Driver.GetInstance();
+
+            var page = new BuyTicketsPage(driver.Instance);
+            page.Open();
+            page.ClickLoginButton();
+            page.EnterLogin("kadac28@gmail.com");
+            page.EnterPassword("kdkddkkd1211");
+            page.ClickLoginButton();
+            Assert.AreEqual(expected: "ANDREY", actual: page.UserName);
+            Assert.IsTrue(page.Authorized);
+
         }
 
         [TestMethod]
         public void OneCanLogout()
         {
-            using (var driver = new Driver())
-            {
-                var page = new BuyTicketsPage(driver.Instance);
-                page.Open();
-                page.ClickLoginButton();
-                page.EnterLogin("kadac28@gmail.com");
-                page.EnterPassword("kdkddkkd1211");
-                page.ClickLoginButton();
-                Assert.AreEqual(expected: "ANDREY", actual: page.UserName);
-                page.ClickLogoutButton();
-                Assert.IsFalse(page.Authorized);
-            }
+            var driver = Driver.GetInstance();
+
+            var page = new BuyTicketsPage(driver.Instance);
+            page.Open();
+            page.ClickLoginButton();
+            page.EnterLogin("kadac28@gmail.com");
+            page.EnterPassword("kdkddkkd1211");
+            page.ClickLoginButton();
+            Assert.AreEqual(expected: "ANDREY", actual: page.UserName);
+            page.ClickLogoutButton();
+            Assert.IsFalse(page.Authorized);
+
         }
     }
 }
